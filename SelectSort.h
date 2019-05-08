2cc630e4a44b6c1e704deb24ea8df6f0eda024b3
YxY <526924057@.com>
2019-05-08 22:03:51 +0800

    fix insert sort

--- SelectSort.h
+++ SelectSort.h
@@ -5,10 +5,8 @@
 //从下标0开始开始遍历
 void insertSort(std::vector<int>& vec) {
     for (int i = 1; i != vec.size(); i++) {
-        for(int j = i ; j >0 ;j--){
-            if(vec[j] < vec[j-1]){
-                std::swap(vec[j], vec[j-1]);
-            }
+        for(int j = i ; j >0 && (vec[j] < vec[j - 1]);j--){
+            std::swap(vec[j], vec[j-1]);
         }
     }
 }
