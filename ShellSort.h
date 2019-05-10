68d69d216b8b832266a66d895f491d2296331d8b
YxY <526924057@.com>
2019-05-10 08:31:59 +0800

    update shell sort remark

--- ShellSort.h
+++ ShellSort.h
@@ -3,8 +3,13 @@
 #include "ApiAlgorithm.h"
 #include <algorithm>
 
-//每次将最小的数放到最前面
-//最低效率: (N^2)/2
+/*
+    希尔排序 > 插入排序
+    希尔排序是基于插入排序的
+    设置一个增量,将数据分成多个组,分别对各组进行排序
+    排序完成后,逐渐减少增量,并重复排序过程,直至增量为1
+*/
+
 void shellSort(std::vector<int>& vec) {
     int n = vec.size();
     int h = 1;
